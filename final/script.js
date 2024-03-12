a83ead4f3e13cd32df72f0cf123e4004d257cb66
Zafar023 <151602063+Zafar023@users.noreply.github.com>
2024-03-12 19:57:44 +0500

    Add files via upload

---
+++ final/script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".btn-pod");
+const box = document.querySelector(".box-pod");
+
+btn.addEventListener("click", () => {
+  box.classList.toggle("active");
+});
+
+const burger = document.querySelector(".burger");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
